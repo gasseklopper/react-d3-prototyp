46ae254add295fbf266040d77a4ed4925fb54775
Markus Morley <markus.morley@sinnerschrader.com>
2024-03-26 09:45:14 +0100

    feat: update barchart

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import BarChartData from './components/BarChart/BarChartData'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -20,7 +20,7 @@ function App() {
           Learn React teee
         </a>
       </header>
-      test
+      test */}
       <BarChartData />
     </div>
   );
--- src/components/BarChart/BarChartData.tsx
+++ src/components/BarChart/BarChartData.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { csv } from 'd3-fetch'
 import { ascending } from 'd3-array'
 import BarChart from './BarChart'
@@ -13,34 +13,59 @@ function type(d: { genre: any; revenue: string | number }) {
 }
 
 function filterData(data: any[]) {
-  return data.filter(d => {
+  return data.filter((d: { revenue: number }) => {
     return d.revenue > 0
   })
 }
 
-function prepareBarChartData(data: any[]) {
+function prepareBarChartData(data: any) {
   // usually more wrangling is required but the example data is simple
   return data
 }
 
 const BarChartData = () => {
-  const [barChartData, setBarChartData] = useState<null | any[]>(null);
-  useEffect(() => {
-    csv('/static/data/barchart.csv', type).then(data => {
+  const [barChartData, setBarChartData] = useState(null);
+
+  const loadBarChartData = (csvFilePath: string) => {
+    csv(csvFilePath, type).then(data => {
       const dataClean = filterData(data)
       setBarChartData(
-        prepareBarChartData(dataClean).sort((a, b) => {
+        prepareBarChartData(dataClean).sort((a: { genre: string | number | boolean | Date | undefined }, b: { genre: string | number | boolean | Date | undefined }) => {
           return ascending(a.genre, b.genre)
         }),
       )
     })
+  }
+
+  useEffect(() => {
+    loadBarChartData('/static/data/barchart.csv');
   }, [])
+
+  const handleLoadButtonClick1 = () => {
+    // Clear old data
+    setBarChartData(null);
+    // Load new data
+    loadBarChartData('/static/data/barchart2.csv');
+  }
+
+  const handleLoadButtonClick2 = () => {
+    // Clear old data
+    setBarChartData(null);
+    // Load new data
+    loadBarChartData('/static/data/barchart.csv');
+  }
 
   if (barChartData === null) {
     return <p>Loading...</p>
   }
 
-  return <BarChart data={barChartData} />
+  return (
+    <div>
+      <button onClick={handleLoadButtonClick2}>Load Barchart 2</button>
+      <button onClick={handleLoadButtonClick1}>Load Barchart 1</button>
+      <BarChart data={barChartData} />
+    </div>
+  )
 }
 
 export default BarChartData
